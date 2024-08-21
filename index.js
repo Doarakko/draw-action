58ed54914b9775472697e42c5483168c29d13d4c
Doarakko <2wua4nlyi@gmail.com>
2024-08-21 23:13:42 +0900

    update api response format

--- index.js
+++ index.js
@@ -19,11 +19,15 @@ async function run() {
         return response.json();
       })
       .then((data) => {
-        const cardName = data.name;
-        if (!(('card_images' in data) && Array.isArray(data.card_images) && data.card_images.length > 0)) {
+        if (!(Array.isArray(data.card_images) && data.length > 0)) {
+          throw new Error("Failed to get card");
+        }
+        const card = data[0];
+        const cardName = card.name;
+        if (!(('card_images' in card) && Array.isArray(card.card_images) && card.card_images.length > 0)) {
           throw new Error("Failed to get card images");
         }
-        const imageUrl = data.card_images[0].image_url;
+        const imageUrl = card.card_images[0].image_url;
 
         octokit.request(
           "POST /repos/{owner}/{repo}/issues/{issue_number}/comments",
